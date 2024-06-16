26879d038f3d9eb3cb0123665b4c739d4ac73326
jeffwcx <jeffwcx@icloud.com>
2024-06-16 18:13:09 +0800

    update button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import { Button } from 'ant-design-vue'
 import TheWelcome from '../components/TheWelcome.vue'
 import { WelcomeDialog } from '../components/dynamicWelcome'
 </script>
 
 <template>
   <main>
+    <Button type="primary" @click="() => WelcomeDialog.open()">Open Welcome</Button>
     <TheWelcome />
-    <button @click="() => WelcomeDialog.open()">Open Welcome</button>
   </main>
 </template>
